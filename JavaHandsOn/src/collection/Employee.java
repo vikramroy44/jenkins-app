1941712106824228011ddc2d22a5d661417ae817
Vikram <vikramroy44@gmail.com>
2023-07-28 12:15:46 +0530

    arrayList and Linkedlist Handson

---
+++ JavaHandsOn/src/collection/Employee.java
@@ -0,0 +1,50 @@
+package collection;
+
+public class Employee {
+	private int id;
+	private String name;
+	private String department;
+	private long salary;
+	
+	
+	
+	public Employee(int id, String name, String department, int salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
+		this.salary = salary;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", department=" + department + ", salary=" + salary + "]";
+	}
+	
+	
+	
+
+}
