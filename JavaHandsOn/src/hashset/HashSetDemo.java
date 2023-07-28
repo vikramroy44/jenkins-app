f509e99bc5f39a7a65a9673a72442d7d119b7533
Vikram <vikramroy44@gmail.com>
2023-07-28 16:41:12 +0530

    HashSet changes

--- JavaHandsOn/src/hashset/HashSetDemo.java
+++ JavaHandsOn/src/hashset/HashSetDemo.java
@@ -1,11 +1,37 @@
 package hashset;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSetDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HashSet hands on");
-
+            Set<String> set = new HashSet<>();
+            set.add("Ram");
+            set.add("Two");
+            set.add("Three");
+            set.add("Four");
+            System.out.println(set);
+            boolean add = set.add("Five");
+            System.out.println(add);
+            System.out.println(set);
+            
+            Set<String> hashSet = new HashSet<>();
+            hashSet.add("Six");
+            hashSet.add("Seven");
+            hashSet.add("Eight");
+            
+            System.out.println("New HashSet: " + hashSet);
+            hashSet.addAll(set);
+            System.out.println(hashSet);
+            
+            Set<String> newset = new HashSet<>();
+            newset.add("Two");
+            newset.add("Four");
+            hashSet.removeAll(newset);
+            System.out.println(hashSet);
 	}
 
 }
