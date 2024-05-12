103db82740974e4be3ba8bd6bb4d084e40b49371
Vikram <vikramroy44@gmail.com>
2024-05-12 14:46:11 +0530

    Second Commit for  Jenkins Git Test
    Added      Jenkins_Test/src/demo/MyTest.java
    
    Signed-off-by: Vikram <vikramroy44@gmail.com>

---
+++ Jenkins_Test/src/demo/MyTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println("Jenkins Test Method Committed");
+
+	}
+
+}
